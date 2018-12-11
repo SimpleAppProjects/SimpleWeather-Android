624ec3a4c246bb7a9dbaa0e443c1d3e422c80cfd
Dave Antoine <bryan2894@gmail.com>
2018-12-10 21:11:24 -0500

    SimpleWeather: update data when package is updated

--- wearapp/src/main/java/com/thewizrd/simpleweather/wearable/WeatherComplicationBroadcastReceiver.java
+++ wearapp/src/main/java/com/thewizrd/simpleweather/wearable/WeatherComplicationBroadcastReceiver.java
@@ -7,6 +7,13 @@ import android.content.Intent;
 public class WeatherComplicationBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
+        if (Intent.ACTION_MY_PACKAGE_REPLACED.equals(intent.getAction())) {
+            WeatherComplicationIntentService.enqueueWork(context,
+                    new Intent(WearableDataListenerService.ACTION_REQUESTWEATHERUPDATE)
+                            .putExtra(WearableDataListenerService.EXTRA_FORCEUPDATE, true));
+            return;
+        }
+
         // Relay intent to WeatherComplicationIntentService
         intent.setClass(context, WeatherComplicationIntentService.class);
         WeatherComplicationIntentService.enqueueWork(context, intent);
