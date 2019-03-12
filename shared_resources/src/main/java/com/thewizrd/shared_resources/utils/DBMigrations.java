d4ffa0dc5ba7a11048c0702eb6834ceb6f78ff7f
Dave Antoine <bryan2894@gmail.com>
2019-03-11 20:09:24 -0400

    SimpleWeather: fix database migrations

--- shared_resources/src/main/java/com/thewizrd/shared_resources/utils/DBMigrations.java
+++ shared_resources/src/main/java/com/thewizrd/shared_resources/utils/DBMigrations.java
@@ -18,15 +18,54 @@ class DBMigrations {
     static final Migration W_MIGRATION_3_4 = new Migration(3, 4) {
         @Override
         public void migrate(@NonNull SupportSQLiteDatabase database) {
-            // Since we didn't alter the table, there's nothing else to do here.
+            // Create the new table
+            database.execSQL(
+                    "CREATE TABLE weatherdata_new (`locationblob` TEXT, `update_time` TEXT, `forecastblob` TEXT, `hrforecastblob` TEXT, `txtforecastblob` TEXT, `conditionblob` TEXT, `atmosphereblob` TEXT, `astronomyblob` TEXT, `precipitationblob` TEXT, `ttl` TEXT, `source` TEXT, `query` TEXT NOT NULL, `locale` TEXT, PRIMARY KEY(`query`))");
+            // Copy the data
+            database.execSQL(
+                    "INSERT INTO weatherdata_new (`locationblob`, `update_time`, `forecastblob`, `hrforecastblob`, `txtforecastblob`, `conditionblob`, `atmosphereblob`, `astronomyblob`, `precipitationblob`, `ttl`, `source`, `query`, `locale`) SELECT `locationblob`, `update_time`, `forecastblob`, `hrforecastblob`, `txtforecastblob`, `conditionblob`, `atmosphereblob`, `astronomyblob`, `precipitationblob`, `ttl`, `source`, `query`, `locale` FROM weatherdata");
+            // Remove the old table
+            database.execSQL("DROP TABLE weatherdata");
+            // Change the table name to the correct one
+            database.execSQL("ALTER TABLE weatherdata_new RENAME TO weatherdata");
+
+            // Create the new table
+            database.execSQL(
+                    "CREATE TABLE weatheralerts_new (`query` TEXT NOT NULL, `weather_alerts` TEXT, PRIMARY KEY(`query`))");
+            // Copy the data
+            database.execSQL(
+                    "INSERT INTO weatheralerts_new (`query`, `weather_alerts`) SELECT `query`, `weather_alerts` FROM weatheralerts");
+            // Remove the old table
+            database.execSQL("DROP TABLE weatheralerts");
+            // Change the table name to the correct one
+            database.execSQL("ALTER TABLE weatheralerts_new RENAME TO weatheralerts");
         }
     };
 
     static final Migration LOC_MIGRATION_3_4 = new Migration(3, 4) {
         @Override
         public void migrate(@NonNull SupportSQLiteDatabase database) {
-            // Add locsource column to locations table
-            database.execSQL("ALTER TABLE locations ADD COLUMN locsource TEXT");
+            // Create the new table
+            database.execSQL(
+                    "CREATE TABLE locations_new (`query` TEXT NOT NULL, `name` TEXT, `latitude` REAL NOT NULL, `longitude` REAL NOT NULL, `tz_long` TEXT, `locationType` INTEGER, `source` TEXT, `locsource` TEXT, PRIMARY KEY(`query`))");
+            // Copy the data
+            database.execSQL(
+                    "INSERT INTO locations_new (`query`, `name`, `latitude`, `longitude`, `tz_long`, `locationType`, `source`) SELECT `query`, `name`, `latitude`, `longitude`, `tz_long`, `locationType`, `source` FROM locations");
+            // Remove the old table
+            database.execSQL("DROP TABLE locations");
+            // Change the table name to the correct one
+            database.execSQL("ALTER TABLE locations_new RENAME TO locations");
+
+            // Create the new table
+            database.execSQL(
+                    "CREATE TABLE favorites_new (`query` TEXT NOT NULL, `position` INTEGER NOT NULL, PRIMARY KEY(`query`))");
+            // Copy the data
+            database.execSQL(
+                    "INSERT INTO favorites_new (`query`, `position`) SELECT `query`, `position` FROM favorites");
+            // Remove the old table
+            database.execSQL("DROP TABLE favorites");
+            // Change the table name to the correct one
+            database.execSQL("ALTER TABLE favorites_new RENAME TO favorites");
         }
     };
 
