535eb232bcc4244566ac706836aa523577ad3f47
Dave Antoine <bryan2894@gmail.com>
2020-12-01 17:39:22 -0500

    WeatherUpdaterWorker: don't fail if we can't update location
    * Continue task if we're unable to get a location update
    * Background access to location may be disabled here; if that's the case continue without failure

--- app/src/main/java/com/thewizrd/simpleweather/services/WeatherUpdaterWorker.java
+++ app/src/main/java/com/thewizrd/simpleweather/services/WeatherUpdaterWorker.java
@@ -9,6 +9,7 @@ import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Looper;
 import android.util.Log;
@@ -183,7 +184,10 @@ public class WeatherUpdaterWorker extends ListenableWorker {
                             updateLocation().get();
                         } catch (ExecutionException | InterruptedException e) {
                             Logger.writeLine(Log.ERROR, e);
-                            return Result.retry();
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q &&
+                                    ContextCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_BACKGROUND_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                                return Result.retry();
+                            }
                         }
                     }
 
