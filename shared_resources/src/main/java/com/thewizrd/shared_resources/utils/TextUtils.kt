c6a6ade4adea700349f9f35fdf760759b5790c80
Dave Antoine <bryan2894@gmail.com>
2021-10-25 00:18:39 -0400

    TextUtils: measure text bounds using StaticLayout

--- shared_resources/src/main/java/com/thewizrd/shared_resources/utils/TextUtils.kt
+++ shared_resources/src/main/java/com/thewizrd/shared_resources/utils/TextUtils.kt
@@ -5,6 +5,9 @@ import android.graphics.Paint
 import android.graphics.Rect
 import android.graphics.Typeface
 import android.os.Build
+import android.text.Layout
+import android.text.StaticLayout
+import android.text.TextPaint
 import android.util.TypedValue
 
 object TextUtils {
@@ -22,7 +25,7 @@ object TextUtils {
         text: CharSequence, textSizeSp: Float,
         typeface: Typeface = Typeface.SANS_SERIF
     ): Rect {
-        val paint = Paint().apply {
+        val paint = TextPaint().apply {
             textSize = TypedValue.applyDimension(
                 TypedValue.COMPLEX_UNIT_SP,
                 textSizeSp,
@@ -33,14 +36,25 @@ object TextUtils {
             style = Paint.Style.FILL
         }
 
-        val rect = Rect()
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            paint.getTextBounds(text, 0, text.length, rect)
+        val width = paint.measureText(text, 0, text.length).toInt()
+        val textLayout = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            StaticLayout.Builder
+                .obtain(text, 0, text.length, paint, width)
+                .setAlignment(Layout.Alignment.ALIGN_CENTER)
+                .setLineSpacing(0f, 1.0f)
+                .setIncludePad(true)
+                .setMaxLines(1)
+                .build()
         } else {
-            paint.getTextBounds(text.toString(), 0, text.length, rect)
+            StaticLayout(
+                text, paint, width,
+                Layout.Alignment.ALIGN_CENTER,
+                1.0f,
+                0f,
+                true
+            )
         }
 
-        return rect
+        return Rect(0, 0, textLayout.width, textLayout.height)
     }
 }
