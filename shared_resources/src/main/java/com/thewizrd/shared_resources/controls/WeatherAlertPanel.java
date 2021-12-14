9cdf37b51dc839356724fd7cde0671b1c5111193
Dave Antoine <thewizrd.dev@gmail.com>
2021-12-13 23:10:26 -0500

    WeatherAlertPanel: make expandable

--- shared_resources/src/main/java/com/thewizrd/shared_resources/controls/WeatherAlertPanel.java
+++ shared_resources/src/main/java/com/thewizrd/shared_resources/controls/WeatherAlertPanel.java
@@ -3,13 +3,23 @@ package com.thewizrd.shared_resources.controls;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 
+import com.thewizrd.shared_resources.R;
 import com.thewizrd.shared_resources.databinding.WeatherAlertPanelBinding;
 
 public class WeatherAlertPanel extends RelativeLayout {
+    /**
+     * State indicating the group is expanded.
+     */
+    private static final int[] GROUP_EXPANDED_STATE_SET = {R.attr.state_expanded};
+
     private WeatherAlertPanelBinding binding;
+
+    private boolean expandable = true;
+    private boolean expanded = false;
 
     public WeatherAlertPanel(Context context) {
         super(context);
@@ -32,21 +42,52 @@ public class WeatherAlertPanel extends RelativeLayout {
 
         this.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
 
-        binding.bodyCard.setVisibility(GONE);
-        binding.expandIcon.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            binding.bodyCard.setVisibility(isChecked ? VISIBLE : GONE);
-        });
-        binding.headerCard.setOnClickListener(v -> {
-            binding.expandIcon.setChecked(!binding.expandIcon.isChecked());
-        });
+        binding.headerCard.setOnClickListener(v -> toggle());
+    }
+
+    public boolean isExpandable() {
+        return expandable;
+    }
+
+    public void setExpandable(boolean expandable) {
+        this.expandable = expandable;
+    }
+
+    public boolean isExpanded() {
+        return expanded;
+    }
+
+    public void setExpanded(boolean expanded) {
+        if (this.expanded != expanded) {
+            toggle();
+        }
+    }
+
+    public void toggle() {
+        if (isExpandable() && isEnabled()) {
+            expanded = !expanded;
+            binding.bodyCard.setVisibility(expanded ? View.VISIBLE : View.GONE);
+            refreshDrawableState();
+        }
     }
 
     public void bindModel(WeatherAlertViewModel model) {
-        binding.setViewModel(model);
-
         // Reset expanded state
-        binding.expandIcon.setChecked(false);
+        setExpandable(true);
+        setExpanded(false);
 
+        binding.setViewModel(model);
         binding.executePendingBindings();
+    }
+
+    @Override
+    protected int[] onCreateDrawableState(int extraSpace) {
+        final int[] drawableState = super.onCreateDrawableState(extraSpace + 1);
+
+        if (isExpanded()) {
+            mergeDrawableStates(drawableState, GROUP_EXPANDED_STATE_SET);
+        }
+
+        return drawableState;
     }
 }
